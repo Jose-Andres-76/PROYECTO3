0d7cbe1af679880db5ef9e309a1d45ee5621523c
gustavocenfotec <galvaradoc@ucenfotec.ac.cr>
2025-07-09 20:41:14 -0600

    General Info Implement
    General Info Implement

---
+++ src/main/java/com/project/demo/rest/auth/emailGeneralInfo.java
@@ -0,0 +1,40 @@
+package com.project.demo.rest.auth;
+
+
+import com.project.demo.logic.entity.email.EmailModel;
+import com.project.demo.logic.entity.http.GlobalResponseHandler;
+import com.project.demo.services.email.EmailService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+
+@RequestMapping("/auth/generalInfo")
+public class emailGeneralInfo {
+
+    @Autowired
+    private EmailService emailService;
+
+
+    @PostMapping
+    public ResponseEntity<?> sendEmailInfoGeneral(@RequestBody Map<String, String> body, HttpServletRequest request) {
+        String email = body.get("email");
+        EmailModel sendmail = new EmailModel();
+        try {
+            sendmail.setSubject("Información General de Eco Asistente AI");
+            sendmail.setText("Somos una aplicación dedicada a la educación del reciclaje, fomentando el conocimiento y el crecimiento familiar.");
+            sendmail.setTo(email);
+            emailService.sendSimpleEmail(sendmail);
+            return new GlobalResponseHandler().handleResponse("Email sent successfully to " + sendmail.getTo(), sendmail, HttpStatus.OK, request);
+        } catch (Exception e) {
+            return new GlobalResponseHandler().handleResponse("Error sending email: " + sendmail.getTo(), sendmail, HttpStatus.INTERNAL_SERVER_ERROR, request);
+        }
+    }
+
+}
