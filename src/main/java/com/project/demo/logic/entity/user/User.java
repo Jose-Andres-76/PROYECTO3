15eefad57050c9a048e324d7277d61f7628f06eb
gustavocenfotec <galvaradoc@ucenfotec.ac.cr>
2025-07-11 15:29:24 -0600

    Delete Order
    Delete Order

--- src/main/java/com/project/demo/logic/entity/user/User.java
+++ src/main/java/com/project/demo/logic/entity/user/User.java
@@ -1,5 +1,4 @@
 package com.project.demo.logic.entity.user;
-import com.project.demo.logic.entity.order.Order;
 import com.project.demo.logic.entity.rol.Role;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -48,8 +47,6 @@ public class User implements UserDetails {
     @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
     private Role role;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-    private List<Order> orders;
 
     // Constructors
     public User() {}
@@ -141,13 +138,6 @@ public class User implements UserDetails {
         return role;
     }
 
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
 
     public User setRole(Role role) {
         this.role = role;
@@ -190,7 +180,6 @@ public class User implements UserDetails {
                 ", publicIdCloudinary='" + publicIdCloudinary + '\'' +
                 ", points=" + points +
                 ", role=" + role +
-                ", orders=" + orders +
                 '}';
     }
 }
