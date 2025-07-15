20dd873783ba834679d50a1c456b8b7172315597
gustavocenfotec <galvaradoc@ucenfotec.ac.cr>
2025-07-14 18:15:39 -0600

    Update Repository Quality
    Update Repository Quality

--- src/main/java/com/project/demo/logic/entity/reward/RewardRepository.java
+++ src/main/java/com/project/demo/logic/entity/reward/RewardRepository.java
@@ -4,7 +4,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+import java.util.List;
+
 @Repository
 public interface RewardRepository extends JpaRepository<Reward, Long> {
+
+    @Query("SELECT r FROM Reward r WHERE r.familyId.id = ?1")
+    List<Reward> findByFamilyId(Long familyId);
+
+    @Query("SELECT r FROM Reward r WHERE r.id = ?1")
+    Optional<Reward> findById(Long id);
+
+    @Query("SELECT r FROM Reward r WHERE r.familyId.id IN (SELECT f.id FROM Family f WHERE f.idFather.id = ?1 OR f.idSon.id = ?1)")
+    List<Reward> findFamilyRewardsByUserId(Long userId);
 
 }
